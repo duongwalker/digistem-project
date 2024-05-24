4c5fcd079593d57a1d8ca4052a95b6d3a777bfce
Joonas Kirsi <joonas@dyn-156-203.public.tut.fi>
2024-05-24 12:39:14 +0300

    Fix digistem logo to remove white background

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ export function NavBar() {
 
   return (
     <div className="navbar w-full flex items-center justify-between py-15 px-7% bg-gray-50 ">
-      <img src={logo} alt='' className='logo cursor-pointer' style={{ width: '30%', maxWidth: '170px' }} />
+      <img src={logo} alt='' className='logo cursor-pointer' style={{ width: '30%', maxWidth: '170px', padding: '5px' }} />
       <ul className="flex-1 list-none text-right font-helvetica-black ">
         <NavItem text='Home' path='/'/>
         <NavItem text='Projects' dropdownItems={['Our project', 'Our team', 'Erasmus+']} path='projects'/>
