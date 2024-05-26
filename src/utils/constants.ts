4c21df2c5d9bef4c0294972a5c8302b6d7ff56d2
Duong Hoang <duong.hoang@tuni.fi>
2024-05-27 02:26:20 +0300

    style: added prettier to all files

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,10 @@
-export const TAMK = 'Tampere University of Applied Sciences (coordinator)'
-export const STU = 'Slovak University of Technology in Bratislava'
-export const UTCB = 'The Technical University of Civil Engineering in Bucharest'
-export const PUOM = 'Universidad Politécnica de Madrid'
+export const TAMK = "Tampere University of Applied Sciences (coordinator)";
+export const STU = "Slovak University of Technology in Bratislava";
+export const UTCB =
+  "The Technical University of Civil Engineering in Bucharest";
+export const PUOM = "Universidad Politécnica de Madrid";
 
 export const MISSIONS = `Our main target is STEM education, specifically for engineering education professionals who teach STEM or other similar subjects. We hope, with this project, we can together increase digital and pedagogical competence of HEI educators and availability of digital resources in STEM academics.\n\n`;
 
-export const sixth_meeting_desc = 'Information about the project was presented also at the SEFI Annual Conference. In the talk: Daniela Velichová, SEFI umbrella for teaching mathematics in engineering.'
-
+export const sixth_meeting_desc =
+  "Information about the project was presented also at the SEFI Annual Conference. In the talk: Daniela Velichová, SEFI umbrella for teaching mathematics in engineering.";
