39a6ab9b15653c19906a742c183dcfe39c71dbe2
Nikola <nikola.simovic@tuni.fi>
2024-06-05 00:43:06 +0300

    Contact/Footer changes

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -1,41 +1,37 @@
-import teamphoto from '/src/assets/teamphoto.jpg';
-import './ContactPage.css'
+import teamphoto from "/src/assets/banner.png";
+import tamkphoto from "/src/assets/teamphoto.jpg";
+import "./ContactPage.css";
 
 const ContactPage = () => {
-    return (
-        <div className="contact-our-team-container">
-            <header className="contact-header">
-                <img src={teamphoto} alt="Tampere University Building"></img>
-            </header>
-            <main>
-                <section className="contact-team-members">
-                    <h1>CONTACT</h1>
-                    <div className="contact-member">
-                        <div className="contact-profile">
-                            <img src={teamphoto} alt="Kirsi-Maria Rinneheimo"></img>
-                            <div className="contact-profile-text">
-                                <h3>Kirsi-Maria Rinneheimo</h3>
-                                <p>M.Sc.</p>
-                                <ul>
-                                    <li>Senior Lecturer of Mathematics</li>
-                                    <li>Over 20 years experience of engineering mathematics' teaching in different engineering disciplines</li>
-                                    <li>Long and extensive experience in working on national and international educational projects as project manager:
-                                        <ul>
-                                            <li>EU funded projects MALog, 2009-2012 (KA3 ICT-505326)</li>
-                                            <li>FutureMath (2015-2018, Erasmus+ KA2 Strategic Partnership)</li>
-                                        </ul>
-                                    </li>
-                                    <li>Current main contact for the DigiSTEM project.</li>
-                                    <li>Email: something@something.something</li>
-                                    <li>Phone: work phone number</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className="contact-our-team-container">
+      <header className="contact-header">
+        <img src={teamphoto} alt="Tampere University Building"></img>
+      </header>
+      <main>
+        <section className="contact-team-members">
+          <h1>CONTACT</h1>
+          <div className="contact-member">
+            <div className="contact-profile">
+              <img src={tamkphoto} alt="Kirsi-Maria Rinneheimo"></img>
+              <div className="contact-profile-text">
+                <h3>TAMK</h3>
+                <ul>
+                  <li>Coordinating University</li>
+                  <li>
+                  Tampere University of Applied Sciences (TAMK) is one of the largest and most popular universities of applied sciences in Finland. Our strengths are multidisciplinary education, creativity, and a strong international dimension. TAMK is a higher education institution oriented towards working life and RDI co-operation.
+</li>
+                  <li>Current main contact for the DigiSTEM project.</li>
+                  <li>Email: tamk@tuni.fi</li>
+                  <li>Phone number: +358 (0)294 5222</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
 
 export default ContactPage
